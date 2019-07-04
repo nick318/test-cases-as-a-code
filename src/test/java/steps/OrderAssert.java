9aa38a96cc8c316650fc597029247178ecb74fb7
Nikita Salomatin <nikita_salomatin@epam.com>
2019-07-04 14:04:56 +0400

    Add allure

---
+++ src/test/java/steps/OrderAssert.java
@@ -0,0 +1,10 @@
+package steps;
+
+import io.qameta.allure.Step;
+
+public class OrderAssert {
+    @Step("Заказ должен быть отображен в таблице")
+    public static void orderShouldBeVisibleInTable() {
+
+    }
+}
--- src/test/java/steps/OrderStep.java
+++ src/test/java/steps/OrderStep.java
@@ -5,40 +5,50 @@ import com.nick318.domain.ClientBase;
 import com.nick318.domain.Dates;
 import com.nick318.domain.Order;
 import com.nick318.domain.Statuses;
+import io.qameta.allure.Step;
 
 public class OrderStep {
+    @Step("Открыть страницу заказов")
     public static OrderStep openOrderPage() {
         return new OrderStep();
     }
 
+    @Step("Нажать на кнопку добавить заказ")
     public OrderStep clickAddOrder() {
         return this;
     }
 
+    @Step("Заполнить поле описание {description}")
     public OrderStep fillDescription(String description) {
         return this;
     }
 
+    @Step("Выбрать клиента")
     public OrderStep chooseClient(Client client) {
         return this;
     }
 
+    @Step("Выбрать дату создания")
     public OrderStep chooseOrderCreateDate(Object dates) {
         return this;
     }
 
+    @Step("Выбрать дату завершения")
     public OrderStep chooseOrderFinishDate(Object dates) {
         return this;
     }
 
+    @Step("Заполнить поле стоимость {price}")
     public OrderStep choosePrice(String price) {
         return this;
     }
 
+    @Step("Выбрать статус {statuses}")
     public OrderStep chooseStatus(Statuses statuses) {
         return this;
     }
 
+    @Step("Добавить заказ")
     public OrderStep addOrder(Order order) {
         //prerequisites
         ClientSteps.openClientPage()
@@ -48,6 +58,7 @@ public class OrderStep {
         return this;
     }
 
+    @Step("Добавить заказ с существующим клиентом")
     public OrderStep addOrderWithExistedUser(Order order) {
         OrderStep.openOrderPage()
                 .clickAddOrder()
